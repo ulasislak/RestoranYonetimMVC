a43a3285d78e0e8ddc77df931580282b880e1e26
ulasislak <ulasislak06@gmail.com>
2025-01-02 21:52:20 +0300

    Add project files.

---
+++ RestoranMenüSiparisMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestoranMenüSiparisMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ RestoranMenüSiparisMVC/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace RestoranMenüSiparisMVC.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredient { get; set; }
+        public double Price { get; set; }
+        public int Unit { get; set; }
+        public string MealPic { get; set; }
+    }
+}
